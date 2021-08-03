97395ac69b210981ff3643825f3074314b1e1355
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2021-08-03 17:54:06 -0400

    menu components

--- src/components/Body/Menu/index.jsx
+++ src/components/Body/Menu/index.jsx
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import items from "../../../data/items";
+import Categories from "./Categories";
+import Items from "./Items";
+import {Route} from "react-router-dom";
+import {NavItems} from "../../HeaderNav/NavItems/NavItems";
+import './index.css'
 
-class Menu extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+function Menu() {
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories] = useState(allCategories);
+
+    const filterItems = (category) => {
+        if (category === 'all') {
+            setMenuItems(items);
+            return;
+        }
+        const newItems = items.filter((item) => item.category === category);
+        setMenuItems(newItems);
+    };
+
+    return (
+        <main>
+            <Route path={NavItems.title} component={Menu}/>
+            <section className='menu section'>
+                <Categories categories={categories} filterItems={filterItems}/>
+                <Items items={menuItems}/>
+            </section>
+        </main>
+    );
 }
+
 
 export default Menu;
---
+++ src/data/items.js
@@ -0,0 +1,31 @@
+import {nanoid} from "nanoid";
+
+const items = [
+    {
+        id:nanoid(),
+        title:'beef',
+        category:'MaLaTang',
+        price:14.99,
+        img: './img/beef.png',
+        description: 'I am beef',
+    },
+    {
+        id:nanoid(),
+        title:'Lamb',
+        category:'MaLaTang',
+        price:14.99,
+        img: './img/lamb.png',
+        description: 'I am beef',
+    },
+    {
+        id:nanoid(),
+        title:'Shrimp Ball',
+        category:'MaLaTang',
+        price:14.99,
+        img: './img/shrimp ball.png',
+        description: 'I am beef',
+    },
+
+    ]
+
+export default items
