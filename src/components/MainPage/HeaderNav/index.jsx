1ec51cb55b00ed7577ec4ab1aa60dc2d012db4bb
Anthony Zhang <anthonyzhang1997@gmail.com>
2021-08-28 10:08:34 -0400

    small changes

--- src/components/MainPage/HeaderNav/index.jsx
+++ src/components/MainPage/HeaderNav/index.jsx
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css';
 import { NavItems } from "./NavItems/NavItems";
 import { Link } from 'react-router-dom';
 
-function HeaderNav (){
+function HeaderNav() {
     const [click, setClick] = useState(false);
-    const [navbar,setNavbar] = useState(false);
-    const [isHome,setIsHome] = useState(false);
+    const [navbar, setNavbar] = useState(false);
+    const [isHome, setIsHome] = useState(false);
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false);
 
-    const changeBackground= () =>{
-        if(window.scrollY >= 50){
+    const changeBackground = () => {
+        if (window.scrollY >= 50) {
             setNavbar(true);
         } else {
             setNavbar(false);
@@ -22,25 +22,30 @@ function HeaderNav (){
     window.addEventListener('scroll', changeBackground)
 
     return (
-            <nav className={`${navbar && styles.active} ${styles.NavItems}`}>
-                <div className={styles.menu_icon} onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}>
-                    </i>
-                </div>
-                <ul className={`${click && styles.active} ${styles.nav_menu}`}>
-                    {NavItems.map((item, index)=> {
-                        return (
-                            <li key={index}>
-                                    <Link onClick={closeMobileMenu} className={styles[item.cName]} to={item.url}>
-                                        {item.title}
-                                    </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
+        <nav className={`${navbar && styles.active} ${styles.NavItems}`}>
+            <div className={styles.menu_icon} onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}>
+                </i>
+            </div>
+            <ul className={`${click && styles.active} ${styles.nav_menu}`}>
+                {NavItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <Link onClick={closeMobileMenu} className={styles[item.cName]} to={item.url}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div className={styles.cart_icon}>
+                <i
+                    className={"fas fa-shopping-cart"}
+                ></i>
+            </div>
+        </nav>
 
-        );
+    );
 }
 
 export default HeaderNav;
