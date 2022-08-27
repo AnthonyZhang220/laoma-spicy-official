cec2f895db8e0da83f53fda58e715836eed8dd9e
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2022-08-26 21:30:15 -0400

    update npm version

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
+import { Suspense, lazy } from "react";
 import HeaderNav from "./components/MainPage/HeaderNav/index";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Menu from "./components/MainPage/Body/Menu";
-import Reservations from "./components/MainPage/Body/Reservations";
-import Home from "./components/MainPage/Body/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/MainPage/Footer';
 import Cart from './components/MainPage/Cart';
 import Blank from './components/MainPage/Blank'
-import Retrieve from "./components/Retrieve";
 import styles from "./App.module.css";
+
+
+const Home = lazy(() => import("./routes/Home"))
+const Menu = lazy(() => import("./routes/Menu"))
+const Reservation = lazy(() => import("./routes/Reservation"))
 
 const App = () => {
     return (
         <Router>
             <div className={styles.body}>
                 <HeaderNav />
-                <Route exact path="/" >
-                    <Home />
-                </Route>
-                <Route path="/Menu" >
-                    <Menu />
-                </Route>
-                <Route path="/Reservations" >
-                    <Reservations />
-                </Route>
-                <Cart />
-                {/* <Retrieve /> */}
-                <Blank />
-                <Footer />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/reservation" element={<Reservation />} />
+                    <Cart />
+                    <Blank />
+                    <Footer />
+                </Routes>
             </div>
         </Router>
     );
